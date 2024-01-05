730e7165a2d66fdcdc9f907a0648f85a7d270828
shakhawat <md.shakhawat@bjitgroup.com>
2024-01-05 12:22:33 +0600

    active route

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,18 +1,38 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./login.module.scss";
 
 const Layout = ({ children }) => {
+  const pathName = usePathname();
+  console.log("pathName ", pathName);
   return (
-    <div>
+    <div className={styles.logIn}>
       <h2>This is Log in Layout</h2>
       <ul>
         <li>
-          <Link href="/login">Log In</Link>
+          <Link
+            href="/login"
+            className={pathName == "/login" && styles.activeRoute}
+          >
+            Log In
+          </Link>
         </li>
         <li>
-          <Link href="/login/loginstudent">Student login</Link>
+          <Link
+            href="/login/loginstudent"
+            className={pathName == "/login/loginstudent" && styles.activeRoute}
+          >
+            Student login
+          </Link>
         </li>
         <li>
-          <Link href="/login/loginteacher">Teacher login</Link>
+          <Link
+            href="/login/loginteacher"
+            className={pathName == "/login/loginteacher" && styles.activeRoute}
+          >
+            Teacher login
+          </Link>
         </li>
       </ul>
       {children}
